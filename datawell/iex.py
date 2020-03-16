762a97c7f03278490be7ad88708df1f6d8dd09c8
Dzmitry Pintusau <dzmitrypintusov@gmail.com>
2020-03-16 17:05:29 +0300

    Pull COMPANY profile from the datasource

--- datawell/iex.py
+++ datawell/iex.py
@@ -12,10 +12,12 @@ class Iex(object):
 
     def __init__(self):
         self.stock_list = []
+        self.company_symbols = []
         self.Logger = app.get_logger(__name__)
         self.Symbols = self.get_stocks()
         datapoints = ['logo', 'company']
         self.Datapoints = dict(zip(datapoints, datapoints))
+        self.Companies = {}
 
     def get_stocks(self):
         """
@@ -32,6 +34,36 @@ class Iex(object):
             message = 'Failed while retrieving stock list!'
             ex = app.AppException(e, message)
             raise ex
+
+    def get_company_data(self, company_symbol=None):
+        """
+        method for IEX which will populate COMPANY data
+        either for all stocks (if no ticker given) or a particular stock (if ticker given).
+        :return: a list of companies or a particular company
+        """
+        try:
+            return self.get_all_companies() if company_symbol is None else self.get_company_by_symbol(company_symbol)
+        except Exception as e:
+            message = 'Failed while retrieving company data!'
+            ex = app.AppException(e, message)
+            raise ex
+
+    def get_all_companies(self):
+        for company_symbol in self.get_company_symbols():
+            self.Companies[company_symbol] = self.get_company_by_symbol(company_symbol)
+        return self.Companies
+
+    def get_company_by_symbol(self, company_symbol: str):
+        uri = app.BASE_API_URL + '/stock/{}/company'.format(company_symbol) + app.API_TOKEN
+        return self.load_from_iex(uri)
+
+    def get_company_symbols(self):
+        """
+        Get all company symbols from stocks list
+        :return: company symbols
+        """
+        self.company_symbols = [stock['symbol'] for stock in self.stock_list]
+        return self.company_symbols
 
     def load_from_iex(self, uri: str):
         """
