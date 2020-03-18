fb7ce3f3d41a917c70c785a6c69845d5ec4c4276
Pavel Bondar <pavel.bondar2@epam.com>
2020-03-18 17:39:55 +0300

    Pull stock CASH FLOW data to the datasource

--- datawell/iex.py
+++ datawell/iex.py
@@ -14,6 +14,7 @@ class Iex(object):
         self.stock_list = []
         self.Logger = app.get_logger(__name__)
         self.Symbols = self.get_stocks()
+        self.cash_flow_list = {}
 
     def get_stocks(self):
         """
@@ -28,6 +29,40 @@ class Iex(object):
 
         except Exception as e:
             message = 'Failed while retrieving stock list!'
+            ex = app.AppException(e, message)
+            raise ex
+
+    def get_cash_flow(self, symbol: str, params=None):
+        """
+        Will return cash flow data for specific symbol on IEX.
+        :param params: dict of uri params for filtering
+        :param symbol: str with symbol from stock obj
+        :return: cash_flow as json obj
+        """
+        params = params if params else {}
+        try:
+            uri = app.BASE_API_URL + 'stock/' + symbol + '/cash-flow' + app.API_TOKEN
+            if params:
+                for key, value in params.items():
+                    uri = uri + '&' + key + '=' + value
+            cash_flow = self.load_from_iex(uri)
+            self.cash_flow_list[symbol] = cash_flow
+            return cash_flow
+        except Exception as ex:
+            message = 'Failed while retrieving cash flow list!'
+            app_ex = app.AppException(ex, message)
+            raise app_ex
+
+    def get_all_cash_flows(self, params=None):
+        """
+        Gets cash_flow for all current stocks
+        :return: List of cash_flow
+        """
+        params = params if params else {}
+        try:
+            return list(map(lambda stock: self.get_cash_flow(stock['symbol'], params), self.get_stocks()))
+        except Exception as e:
+            message = 'Failed while retrieving cash_flow list!'
             ex = app.AppException(e, message)
             raise ex
 
