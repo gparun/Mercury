8760b1fe630bf60564c4f4431621148d025f1525
Denis Petelin <d.petelin@icloud.com>
2020-03-26 20:01:52 -0500

    refactor(persistence): add example of the error handling (#15)
    Added an example which demonstrates:
    - What is guard clause
    - How to use exceptions for the normal flow
    - How to ship output via Results

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,7 @@ class ActionStatus(Enum):
 class Results:
     def __init__(self):
         self.ActionStatus: ActionStatus = ActionStatus.ERROR
-        self.Results = []
+        self.Results = any()
 
 
 class AppException(Exception):
--- persistence/DynamoStore.py
+++ persistence/DynamoStore.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from app import Results, ActionStatus, AppException
+
 
 class DynamoStore:
     def __init__(self):
@@ -30,9 +32,32 @@ class DynamoStore:
         """
         pass
 
-    def remove_empty_strings(dict_to_clean: dict):
+    def remove_empty_strings(dict_to_clean: dict) -> Results:
         """
         Removes all the empty key+value pairs from the dict you give it; use to clean up dicts before persisting them to the DynamoDB
         :param dict_to_clean: as dict()
-        :return: only non-empty key+value pairs from the source dict as dict()
+        :return: only non-empty key+value pairs from the source dict as dict() inside Results
         """
+        try:
+            # start with your guard clauses - thats a standard situation you have to handle in your error handling
+            assert type(dict_to_clean) is dict
+
+            # here comes processing...
+            cleaned_dict = dict()
+
+            # now you are ready to ship back...
+            output = Results()
+            output.ActionStatus = ActionStatus.SUCCESS
+            output.Results = cleaned_dict
+            return output
+
+        # here we have exceptions we KNOW we can encounter...
+        except AssertionError:
+            output = Results()
+            output.Results = "You have to pass dict to this method!"
+
+        # and this analog of *nix panic(*message)...
+        except Exception as e:
+            catastrophic_exception = AppException(ex=e, message='Catastrophic failure when trying to clean up dict '
+                                                                'for the Dynamo!')
+            raise catastrophic_exception
