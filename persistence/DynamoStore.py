d0adce5973d5340fb38521f489d56cf44bf2aaae
MaksimDragun <MaksimDragun@live.com>
2020-04-09 05:10:39 +0300

    Merge pull request #28
    * feat(persistence): implement DynamoStore.clean_table method
    
    * feat(DynamoStore)-implement DynamoStore.clean_table method
    
    * feat(persistence): implement DynamoStore.clean_table method
    
    * feat(DynamoStore)-implement DynamoStore.clean_table method
    
    * Merge remote-tracking branch 'origin/feature/16/DynamoStore_clean_table' into feature/16/DynamoStore_clean_table
    
    # Conflicts:
    #	persistence/DynamoStore.py
    
    * feat(DynamoStore)-implement DynamoStore.clean_table method

--- persistence/DynamoStore.py
+++ persistence/DynamoStore.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from typing import Generator
+
 from boto3.exceptions import RetriesExceededError
 from botocore.exceptions import ClientError
 import app
@@ -9,9 +11,9 @@ import boto3
 from boto3.dynamodb.conditions import Key
 
 
-
 class DynamoStore:
     def __init__(self):
+        self.Logger = app.get_logger(__name__)
         self.dynamoDb = boto3.resource("dynamodb")
         self.table = self.dynamoDb.Table(TABLE)
 
@@ -58,13 +60,144 @@ class DynamoStore:
                                                                 'for the Dynamo!')
             raise catastrophic_exception
 
-    def clean_table(self, symbols_to_remove: list):
+    def clean_table(self, symbols_to_remove: list) -> Results:
         """
         Use this one to either clean specific stocks from the db or clean up the db if its small.
         :param symbols_to_remove: list of dicts each containing 'symbol' string
-        :returns: number of the elements removed as int, 0 if not found, AppException if AWS Error: No access etc
+        :returns: number of the elements removed as int, 0 if not found, -1 if full table is cleaned,
+         AppException if AWS Error: No access etc
         """
-        pass
+        output = Results()
+        try:
+            assert type(symbols_to_remove) is list
+            if symbols_to_remove:
+                self.Logger.info(f"Deleting {symbols_to_remove}")
+                total_deleted_items = self._delete_symbols(symbols_to_remove)
+                output.ActionStatus = ActionStatus.SUCCESS
+                output.Results = total_deleted_items
+            else:
+                self.Logger.info('Nothing is specified to delete, so deleting the entire table')
+                self._recreate_table()
+                output.ActionStatus = ActionStatus.SUCCESS
+                output.Results = -1
+        except AssertionError:
+            output.Results = "You have to pass a list of dict to the method!"
+        except ClientError as e:
+            raise AppException(e, "An error has occurred while interacting with Dynamo")
+        return output
+
+    def _delete_symbols(self, symbols_to_remove: list) -> int:
+        """
+        Use this method to delete symbols using the batch_writer.
+
+        :param symbols_to_remove: what to delete.
+        :return: total deleted items.
+        """
+        total_items_to_delete: int = 0
+        with self.table.batch_writer() as batch:
+            for symbol in symbols_to_remove:
+                symbol_to_remove: str = symbol.get("symbol")
+                if not symbol_to_remove:
+                    continue
+                for page in self._query_by_page(symbol_to_remove):
+                    total_items_to_delete += page['Count']
+                    for item in page["Items"]:
+                        batch.delete_item(
+                            Key={
+                                "symbol": item["symbol"],
+                                "date": item["date"]
+                            }
+                        )
+        return total_items_to_delete
+
+    def _query_by_page(self, symbol: str) -> Generator[dict, None, None]:
+        """Use this method to query all symbol related records using pagination.
+
+        :param symbol: what to search.
+        :return: an iterable of pages. There is a python generator to perform lazy evaluated queries.
+        """
+        last_evaluated_key: dict = {}
+        while True:
+            query_params = {
+                'KeyConditionExpression': Key('symbol').eq(symbol),
+                'ProjectionExpression': '#symbol, #date',
+                'ExpressionAttributeNames': {
+                    "#symbol": "symbol",
+                    "#date": "date",
+                },
+            }
+            if last_evaluated_key:
+                query_params['ExclusiveStartKey'] = last_evaluated_key
+            page: dict = self.table.query(**query_params)
+
+            yield page
+
+            last_evaluated_key = page.get('LastEvaluatedKey')
+            if not last_evaluated_key:
+                return
+
+    def _recreate_table(self) -> None:
+        """
+        Use this method to clean the table quickly. It drops and creates a new table with the same schema.
+        """
+        self.Logger.info('Delete the table')
+        self.table.delete()
+        self.Logger.info('Wait until the table is deleted')
+        self.table.meta.client.get_waiter('table_not_exists').wait(TableName=app.TABLE)
+
+        self.Logger.info('Create a new table.')
+        self.table = self.dynamoDb.create_table(
+            TableName=app.TABLE,
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'symbol',
+                    'AttributeType': 'S',
+                },
+                {
+                    'AttributeName': 'date',
+                    'AttributeType': 'S',
+                }
+            ],
+            KeySchema=[
+                {
+                    'AttributeName': 'symbol',
+                    'KeyType': 'HASH',
+                },
+                {
+                    'AttributeName': 'date',
+                    'KeyType': 'RANGE',
+                }
+            ],
+            GlobalSecondaryIndexes=[
+                {
+                    'IndexName': 'date-symbol-index',
+                    'KeySchema': [
+                        {
+                            'AttributeName': 'date',
+                            'KeyType': 'HASH',
+                        },
+                        {
+                            'AttributeName': 'symbol',
+                            'KeyType': 'RANGE',
+                        }
+                    ],
+                    'Projection': {
+                        'ProjectionType': 'ALL',
+                    },
+                    'ProvisionedThroughput': {
+                        'ReadCapacityUnits': 5,
+                        'WriteCapacityUnits': 100,
+                    }
+                },
+            ],
+            BillingMode='PROVISIONED',
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 100,
+            },
+        )
+        self.Logger.info('Wait until the table exists.')
+        self.table.meta.client.get_waiter('table_exists').wait(TableName=app.TABLE)
 
     def remove_empty_strings(self, dict_to_clean: dict) -> Results:
         """
