4e94ec1eef45a93a774ae48831ba847b4531ce19
olga.kazakova <olga.kazakova@thomsonreuters.com>
2020-04-14 23:15:02 +0300

    feat(data_retrieval): use batches to retrieve companies(#32)

--- datawell/iex.py
+++ datawell/iex.py
@@ -7,21 +7,16 @@ from decimal import Decimal
 import requests
 import app
 from datawell.decorators import retry
-from urllib.parse import urlencode
-
+from typing import List
 
 class Iex(object):
 
-    def __init__(self):
+    SYMBOL_BATCH_SIZE = 100
+
+    def __init__(self, datapoints: List[str] = None):
         self.Logger = app.get_logger(__name__)
+        self.datapoints = datapoints
         self.Symbols = self.get_stocks()
-        self.pull_all_books()
-        self.get_companies()
-        self.populate_financials()
-        self.populate_dividends()
-        for stock in self.Symbols:
-            stock['cash-flow'] = self.get_cash_flow(stock['symbol'])
-        self.populate_advanced_stats()
 
     def get_stocks(self):
         """
@@ -30,62 +25,33 @@ class Iex(object):
         """
         try:
             # basically we create a market snapshot
-            uri = f'{app.BASE_API_URL}ref-data/Iex/symbols/{app.API_TOKEN}'
-            results: app.Results = self.load_from_iex(uri)
+            results: app.Results = self.load_from_iex("ref-data/Iex/symbols/")
             return results.Results
         except Exception as e:
             message = 'Failed while retrieving stock list!'
             ex = app.AppException(e, message)
             raise ex
 
-    def pull_all_books(self):
-        """
-        Pulls all books to Symbols dict
-        """
-        for symbol in self.Symbols:
-            uri = f"{app.BASE_API_URL}stock/{symbol['symbol']}/book{app.API_TOKEN}"
-            results = self.load_from_iex(uri)
-            if results.ActionStatus == app.ActionStatus.SUCCESS:
-                symbol['book'] = results.Results
-
-    def get_companies(self):
-        try:
-            for symbol in self.Symbols:
-                uri = f"{app.BASE_API_URL}stock/{symbol['symbol']}/company{app.API_TOKEN}"
-                results = self.load_from_iex(uri)
-                if results.ActionStatus == app.ActionStatus.SUCCESS:
-                    symbol['company'] = results.Results
-        except Exception as e:
-            message = 'Failed while retrieving company list!'
-            ex = app.AppException(e, message)
-            raise ex
-
-    def get_cash_flow(self, symbol: str, params=None):
-        """
-        Will return cash flow data for specific symbol on IEX.
-        :param params: dict of uri params for filtering
-        :param symbol: str with symbol from stock obj
-        :return: cash_flow as json obj
-        """
-        params = params if params else {}
-        params['token'] = app.API_TOKEN
-        uri = app.BASE_API_URL + 'stock/' + symbol + '/cash-flow?'
-        uri += urlencode(params)
-        cash_flow = ""
-        results = self.load_from_iex(uri)
-        if results.ActionStatus == app.ActionStatus.SUCCESS:
-            cash_flow = results.Results
-        return cash_flow
-
     @retry(delay=5, max_delay=30)
-    def load_from_iex(self, uri: str) -> app.Results:
+    def load_from_iex(self, url_path: str, symbols: List[str] = None, datapoints: List[str] = None) -> app.Results:
         """
-        Connects to the specified IEX endpoint and gets the data you requested.
-        :type uri: str with the endpoint to query
-        :return Dict() with the answer from the endpoint, Exception otherwise
+        Connects to the IEX endpoint and gets the data you requested
+        :param symbols: list of Symbols you need to get data for. Note that symbols count must not exceed 100 per request
+        :param datapoints: list of datapoints you need to get data for. Note that datapoints count must not exceed 10 per request
+        :return: Dict() with the answer from the endpoint
         """
-        self.Logger.info(f'Now retrieving from {uri}')
-        response = requests.get(uri)
+        assert symbols is None or len(symbols) <= 100, 'Load from IEX error: symbols count must not exceed 100 per request'
+        assert datapoints is None or len(datapoints) <= 10, 'Load from IEX error: datapoints count must not exceed 10 per request'
+
+        self.Logger.info(f"Now retrieving from {app.BASE_API_URL}{url_path}")
+
+        request_params = {"token": app.API_TOKEN}
+        if datapoints is not None:
+            request_params['symbols'] = f"{','.join(symbols)}"
+        if datapoints is not None:
+            request_params['types'] = f"{','.join(datapoints)}"
+
+        response = requests.get(f"{app.BASE_API_URL}{url_path}", params=request_params)
         results = app.Results()
 
         if response.status_code == 200:
@@ -96,76 +62,33 @@ class Iex(object):
         else:
             error = response.status_code
             self.Logger.error(
-                f'Encountered an error: {error} ({response.text}) while retrieving {uri}')
+                f"Encountered an error: {error} ({response.text}) while retrieving {app.BASE_API_URL}{url_path}")
+            if symbols is not None:
+                self.Logger.info(f"Failed Symbols: {','.join(symbols)}")
+            if datapoints is not None:
+                self.Logger.info(f"Failed Datapoints: {','.join(datapoints)}")
+
             results.Results = error
 
         return results
 
-    def populate_financials(self, ticker: dict = None) -> None:
-        if ticker:
-            self.populate_ticker_financials(ticker)
-        else:
-            for ticker in self.Symbols:
-                self.populate_ticker_financials(ticker)
-
-    def populate_ticker_financials(self, ticker: dict) -> None:
-        url = f'{app.BASE_API_URL}stock/{ticker["symbol"]}/financials/{app.API_TOKEN}'
-        results = self.load_from_iex(url)
-        if results.ActionStatus == app.ActionStatus.SUCCESS:
-            ticker['financials'] = results.Results
-
-    def populate_advanced_stats(self) -> app.ActionStatus:
-        try:
-            for stock in self.Symbols:
-                self.populate_advanced_stats_for_stock(stock)
-            return app.ActionStatus.SUCCESS
-        except Exception as e:
-            message = 'Failed while retrieving advanced-stats data'
-            ex = app.AppException(e, message)
-            raise ex
-
-    def populate_advanced_stats_for_stock(self, stock: dict) -> app.ActionStatus:
-        try:
-            url = self.generate_symbol_info_service_url(stock['symbol'], 'advanced-stats')
-            results = self.load_from_iex(url)
-            if results.ActionStatus == app.ActionStatus.SUCCESS:
-                stock['advanced-stats'] = results.Results
-            return app.ActionStatus.SUCCESS
-        except Exception as e:
-            self.Logger.exception(f'Unable to load advanced-stats data for stock: {stock["symbol"]}')
-            return app.ActionStatus.ERROR
-
-    def generate_symbol_info_service_url(self, symbol: str, service_name: str) -> str:
-        assert symbol and symbol.strip(), 'IEX symbol must be a non empty string'
-        assert service_name and service_name.strip(), 'IEX service name must be a non empty string'
-        return f'{app.BASE_API_URL}stock/{symbol}/{service_name}/{app.API_TOKEN}'
-
-    def populate_dividends(self, ticker: dict = None, range: str = '1y'):
+    def load_symbols_datapoint_info(self, datapoints: List[str]):
         """
-        Adds dividends item to the dict specified by ticker argument.
-        If the ticker is not specified, then walks though all company dicts in the stock list to add dividends.
-        :param ticker: dictionary to add dividends to
-        :param range: dividends time range
+        Splits Symbols into the 100 item bathes and get IEX data for each batch and defined datapoints.
+        Updates Symbols with retrieved datapoint data.
+        :param datapoints: list of datapoint names. Note that datapoints count must not exceed 10
         """
-        try:
-            if ticker:
-                ticker['dividends'] = self.load_dividends(ticker['symbol'], range)
-            else:
-                for ticker in self.Symbols:
-                    self.populate_dividends(ticker, range)
-        except Exception as e:
-            raise app.AppException(e, 'Exception while populating dividends')
+        symbols_dict = {symbol["symbol"]: symbol for symbol in self.Symbols}
+        symbol_names = list(symbols_dict.keys())
+        for i in range(0, len(symbol_names), self.SYMBOL_BATCH_SIZE):
+            result = self.load_from_iex("stock/market/batch", symbol_names[i: i + self.SYMBOL_BATCH_SIZE], datapoints)
+            if result.ActionStatus == app.ActionStatus.SUCCESS:
+                self.update_symbols(symbols_dict, result.Results)
+        self.Symbols = list(symbols_dict.values())
 
-    def load_dividends(self, company_symbol: str, range_: str = '1y'):
-        """
-        Makes API call to retrieve dividends for the company and time range
-        :param company_symbol: company identifier
-        :param range_: time range to retrieve dividends for
-        """
-        url = f'{app.BASE_API_URL}stock/{company_symbol}/dividends/{range_}/{app.API_TOKEN}'
-        results = self.load_from_iex(url)
-        if results.ActionStatus == app.ActionStatus.SUCCESS:
-            return results.Results
-        else:
-            return ""
-
+    def update_symbols(self, symbols_dict: dict, data_points_data: dict):
+        for symbol_name in data_points_data.keys():
+            symbol = symbols_dict[symbol_name]
+            point_symbol = data_points_data[symbol_name]
+            for point_name in point_symbol.keys():
+                symbol[point_name] = point_symbol[point_name]
