906e7322ab141540bc5798e710a573f1247eb638
olga.kazakova <olga.kazakova@thomsonreuters.com>
2020-04-15 13:07:11 +0300

    feat(data_retrieval): prettify the code(#32)

--- datawell/iex.py
+++ datawell/iex.py
@@ -33,23 +33,18 @@ class Iex(object):
             raise ex
 
     @retry(delay=5, max_delay=30)
-    def load_from_iex(self, url_path: str, symbols: List[str] = None, datapoints: List[str] = None) -> app.Results:
+    def load_from_iex(self, url_path: str, params: dict = None) -> app.Results:
         """
         Connects to the IEX endpoint and gets the data you requested
-        :param symbols: list of Symbols you need to get data for. Note that symbols count must not exceed 100 per request
-        :param datapoints: list of datapoints you need to get data for. Note that datapoints count must not exceed 10 per request
+        :param url_path: service path
+        :param params: extra parameters to include to url
         :return: Dict() with the answer from the endpoint
         """
-        assert symbols is None or len(symbols) <= 100, 'Load from IEX error: symbols count must not exceed 100 per request'
-        assert datapoints is None or len(datapoints) <= 10, 'Load from IEX error: datapoints count must not exceed 10 per request'
-
         self.Logger.info(f"Now retrieving from {app.BASE_API_URL}{url_path}")
 
         request_params = {"token": app.API_TOKEN}
-        if datapoints is not None:
-            request_params['symbols'] = f"{','.join(symbols)}"
-        if datapoints is not None:
-            request_params['types'] = f"{','.join(datapoints)}"
+        if params is not None:
+            request_params.update(params)
 
         response = requests.get(f"{app.BASE_API_URL}{url_path}", params=request_params)
         results = app.Results()
@@ -63,28 +58,28 @@ class Iex(object):
             error = response.status_code
             self.Logger.error(
                 f"Encountered an error: {error} ({response.text}) while retrieving {app.BASE_API_URL}{url_path}")
-            if symbols is not None:
-                self.Logger.info(f"Failed Symbols: {','.join(symbols)}")
-            if datapoints is not None:
-                self.Logger.info(f"Failed Datapoints: {','.join(datapoints)}")
-
+            if params is not None:
+                self.Logger.info(f"Failed parameters: {params}")
             results.Results = error
 
         return results
 
-    def load_symbols_datapoint_info(self, datapoints: List[str]):
+    def load_symbols_datapoints_info(self, datapoints: List[str]):
         """
         Splits Symbols into the 100 item bathes and get IEX data for each batch and defined datapoints.
         Updates Symbols with retrieved datapoint data.
         :param datapoints: list of datapoint names. Note that datapoints count must not exceed 10
         """
         symbols_dict = {symbol["symbol"]: symbol for symbol in self.Symbols}
-        symbol_names = list(symbols_dict.keys())
-        for i in range(0, len(symbol_names), self.SYMBOL_BATCH_SIZE):
-            result = self.load_from_iex("stock/market/batch", symbol_names[i: i + self.SYMBOL_BATCH_SIZE], datapoints)
+        for symbols_batch in self._batchify(list(symbols_dict.keys())):
+            result = self.load_symbols_from_iex("stock/market/batch", symbols_batch, datapoints)
             if result.ActionStatus == app.ActionStatus.SUCCESS:
                 self.update_symbols(symbols_dict, result.Results)
         self.Symbols = list(symbols_dict.values())
+
+    def _batchify(self, lst):
+        for i in range(0, len(lst), self.SYMBOL_BATCH_SIZE):
+            yield lst[i:i + self.SYMBOL_BATCH_SIZE]
 
     def update_symbols(self, symbols_dict: dict, data_points_data: dict):
         for symbol_name in data_points_data.keys():
@@ -92,3 +87,15 @@ class Iex(object):
             point_symbol = data_points_data[symbol_name]
             for point_name in point_symbol.keys():
                 symbol[point_name] = point_symbol[point_name]
+
+    def load_symbols_from_iex(self, url_path: str, symbols: List[str], datapoints: List[str]):
+        assert len(symbols) <= 100, 'Load from IEX error: symbols count must not exceed 100 per request'
+        assert len(datapoints) <= 10, 'Load from IEX error: datapoints count must not exceed 10 per request'
+
+        request_params = {}
+        if len(symbols) > 0:
+            request_params['symbols'] = ','.join(symbols)
+        if len(datapoints) > 0:
+            request_params['types'] = ','.join(datapoints)
+        result = self.load_from_iex(url_path, request_params)
+        return result
